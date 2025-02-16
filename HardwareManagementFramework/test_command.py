dda7fab0a236bb605ee93b75396594640a0cf4e3
Your Name <you@example.com>
2025-02-17 01:04:58 +0530

    ss

--- HardwareManagementFramework/test_command.py
+++ HardwareManagementFramework/test_command.py
@@ -1,5 +1,6 @@
 from Command.Command import Command
 from dotenv import load_dotenv
+load_dotenv()
 myCommand = Command()
 resp = myCommand.send_command_to_MC(command="1111", port="/dev/ttyUSB0", desc="All On")
 print(resp)
