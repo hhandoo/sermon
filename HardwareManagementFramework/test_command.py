4f366b438a215eafac9a3d94041e49fbeb54ddd3
Your Name <you@example.com>
2025-02-16 23:27:57 +0530

    test

--- HardwareManagementFramework/test_command.py
+++ HardwareManagementFramework/test_command.py
@@ -1,5 +1,5 @@
 from Command.Command import Command
 
 myCommand = Command()
-resp = myCommand.send_command_to_MC(command="1111", port="/dev/ttyUSB0")
+resp = myCommand.send_command_to_MC(command="1111", port="/dev/ttyUSB0", desc="All On")
 print(resp)
