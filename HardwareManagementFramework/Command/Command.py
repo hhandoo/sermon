77ba6cb5e6873270ff6c18e429ccd2eaee399bd2
Your Name <you@example.com>
2025-02-16 22:58:55 +0530

    test

--- HardwareManagementFramework/Command/Command.py
+++ HardwareManagementFramework/Command/Command.py
@@ -1,10 +1,12 @@
-import socket
 from SerialCommunication.SerialCommunication import SerialCommunication
+from DatabaseController.DatabaseController import DatabaseController
+from NotificationSystem.NotificationSystem import NotificationSystem
 
 
 class Command:
     def __init__(self):
-        pass
+        self._DatabaseController = DatabaseController()
+        self._NotificationSystem = NotificationSystem()
 
     def send_command_to_MC(self, command: str, port: str = None) -> str:
         if command != None and command != "":
@@ -12,16 +14,20 @@ class Command:
                 if port:
                     with SerialCommunication(port=port) as comm:
                         response = comm.send_command(f"{command}\n")
+                        updated_state = response[0]
+                        print(response)
                         return response
                 else:
                     with SerialCommunication() as comm:
                         response = comm.send_command(f"{command}\n")
+                        updated_state = response[0]
+                        print(response)
                         return response
             except Exception as e:
                 print(f"Error during communication: {e}")
 
     def get_last_command(self) -> str:
-        pass
+        return self._DatabaseController.get_active_appliance_control()
 
     def save_command(self) -> bool:
         pass
--- HardwareManagementFramework/test_command.py
+++ HardwareManagementFramework/test_command.py
@@ -1,7 +1,5 @@
 from Command.Command import Command
 
 myCommand = Command()
-
 resp = myCommand.send_command_to_MC(command="1111", port="/dev/ttyUSB0")
-
 print(resp)
