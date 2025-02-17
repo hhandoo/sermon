8b98e7caeadc3ce7b7739447adbac10aedef1e27
Your Name <you@example.com>
2025-02-17 23:34:23 +0530

    files

--- triggers/on_demand_router_restart.sh
+++ triggers/on_demand_router_restart.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-if [[ -z "$RUNNING_IN_BACKGROUND" ]]; then
-    export RUNNING_IN_BACKGROUND=1
-    nohup "$0" "$@" > /dev/null 2>&1 & disown
-    exit
-fi
-
 CURRENT_DIR=$(pwd)
 date
 echo "Process Started !!"
 
 cd /home/sv_admin/production/sermon || exit
+
 source .venv/bin/activate
 cd HardwareManagementFramework || exit
-
 nohup python3 main.py --on-demand-router-restart > /dev/null 2>&1 & disown
 
 cd ..
