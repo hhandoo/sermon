2b0f367f49cf5b7326bb956b2974e04c8722d468
Your Name <you@example.com>
2025-02-17 23:41:34 +0530

    ff

--- triggers/on_demand_router_restart.sh
+++ triggers/on_demand_router_restart.sh
@@ -8,7 +8,7 @@ cd /home/sv_admin/production/sermon || exit
 
 source .venv/bin/activate
 cd HardwareManagementFramework || exit
-python3 main.py --on-demand-router-restart
+nohup python3 main.py --on-demand-router-restart > /dev/null 2>&1 & disown
 
 cd ..
 deactivate
