9f37d84faadc9b3a7ef986236c070d5547b6f23e
Your Name <you@example.com>
2025-02-17 22:24:18 +0530

    change

---
+++ triggers/on_demand_router_restart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+CURRENT_DIR=$(pwd)
+date
+echo "Process Started !!"
+cd /home/sv_admin/production/sermon
+
+
+
+source .venv/bin/activate
+cd HardwareManagementFramework
+python3 main.py --on-demand-router-restart
+cd ..
+deactivate
+echo "Process Ended !!"
+cd "$CURRENT_DIR"
+
