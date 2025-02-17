bbc413ce994fa97f3167eea85c119b8020eb6525
Your Name <you@example.com>
2025-02-17 23:53:53 +0530

    ss

--- triggers/on_demand_router_restart.sh
+++ triggers/on_demand_router_restart.sh
@@ -8,7 +8,11 @@ cd /home/sv_admin/production/sermon || exit
 
 source .venv/bin/activate
 cd HardwareManagementFramework || exit
-nohup python3 main.py --on-demand-router-restart > /dev/null 2>&1 & disown
+
+# Trigger router restart (power off, then power on)
+nohup python3 main.py --on-demand-router-restart > /dev/null 2>&1 &
+disown
+sleep 10
 
 cd ..
 deactivate
