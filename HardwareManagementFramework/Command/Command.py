28da1e2d7ede6d67ad6d1e2f836f2459ae598ccb
Your Name <you@example.com>
2025-02-17 23:45:56 +0530

    ss

--- HardwareManagementFramework/Command/Command.py
+++ HardwareManagementFramework/Command/Command.py
@@ -152,5 +152,5 @@ class Command:
         time.sleep(1)
 
         self.send_command_to_MC(
-            "1111", desc="GX router and 8 Port swith restarted.", send_notif=True
+            "1111", desc="GX router and 8 Port swith restarted.", send_notif=False
         )
