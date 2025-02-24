e4ff28fe9cb79836796ffc77ea02258393a93496
Harsh Handoo <handoo.harsh@gmail.com>
2025-02-24 20:51:02 +0530

    Update on_demand_router_restart.sh

--- triggers/on_demand_router_restart.sh
+++ triggers/on_demand_router_restart.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "0000" > /dev/ttyUSB0
+DEVICE="/dev/ttyUSB0"
+BAUD=115200
+SESSION_NAME="nodemcu_session"
 
+# Start a detached screen session
+screen -dmS $SESSION_NAME $DEVICE $BAUD
+sleep 1  # Give screen some time to start
+
+# Send "0000" followed by Enter
+screen -S $SESSION_NAME -X stuff "0000\r"
+
+# Wait 30 seconds
 sleep 30
 
-echo "0011" > /dev/ttyUSB0
+# Send "0011" followed by Enter
+screen -S $SESSION_NAME -X stuff "0011\r"
 
-sleep 30
+# Optional: Detach from session (keeps screen running in the background)
+screen -S $SESSION_NAME -X detach
